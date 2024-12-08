7f7e41c2a92eb50cc792d707349e52757673d8b3
FMGustafsson <marcus.gustafsson@hotmail.co.uk>
2024-12-08 15:10:07 +0000

    Add everything working

--- main.py
+++ main.py
@@ -53,11 +53,23 @@ async def start_game(interaction: discord.Interaction):
             players[i] = players[i][2:]
             players[i] = players[i][:-1]
             print(players)
-            user = await client.fetch_user(players[i])
-            await user.send(file=discord.File('blankdrawing.png'))
-        await asyncio.sleep(1)
-        await download_image_and_send(players)
-    await ask_for_prompt(players)
+        round = 2
+        iteration = 1
+        await ask_for_initial_prompt(players)
+        await send_blank(players)
+        await asyncio.sleep(30)
+        await download_initial_image_and_send(players)
+        while True:
+            round = round+1
+            if round>len(players):
+                break
+            await ask_for_prompt(players, iteration)
+            round = round+1
+            if round>len(players):
+                break
+            await send_blank(players)
+            await asyncio.sleep(30)
+            await download_image_and_send(players, iteration)
 
 
 @client.tree.command(description="Describe what the bot does")
@@ -91,7 +103,7 @@ async def on_ready():  #  Called when internal cache is loaded
     pass
 
 @client.event
-async def download_image_and_send(players):
+async def download_initial_image_and_send(players):
     if not os.path.exists('temp'):
         os.mkdir('temp')
     for player in players:
@@ -104,7 +116,7 @@ async def download_image_and_send(players):
                 print("Balls")
                 return
             else: # If there is it gets the filename from message.attachments
-                imageName = "temp/image_" + player + "_" + ".png"
+                imageName = "temp/image_" + player + "_" + "0.png"
                 await latestmessage.attachments[0].save(imageName) # saves the file
                 print("wins")
         else:
@@ -112,26 +124,91 @@ async def download_image_and_send(players):
             print("Fuck")
     i=1
     for player in players:
-        prevImage = "temp/image_" + player + "_" + ".png"
+        prevImage = "temp/image_" + player + "_" + "0.png"
         user = await client.fetch_user(players[i])
         await user.send(file=discord.File(prevImage))
         i=i+1
         if i>=len(players):
             i=0
 
+@client.event
+async def download_image_and_send(players, round):
+    for player in players:
+        user = await client.fetch_user(player)
+        if user:
+            # found the user
+            messages = [message async for message in user.history(limit=1)]
+            latestmessage = messages[0]
+            if not latestmessage.attachments: # Checks if there is an attachment on the message
+                print("Balls")
+                return
+            else: # If there is it gets the filename from message.attachments
+                imageName = "temp/image_" + player + "_" + str(round) + ".png"
+                await latestmessage.attachments[0].save(imageName) # saves the file
+                print("wins")
+        else:
+            # Not found the user
+            print("Fuck")
+    i=1
+    for player in players:
+        prevImage = "temp/image_" + player + "_" + str(round) + ".png"
+        user = await client.fetch_user(players[i])
+        await user.send(file=discord.File(prevImage))
+        i=i+1
+        if i>=len(players):
+            i=0
 
-async def ask_for_prompt(players):
+async def send_blank(players):
+    for player in players:
+        user = await client.fetch_user(player)
+        await user.send(file=discord.File('blankdrawing.png'))
+
+async def ask_for_initial_prompt(players):
     for player in players:
         user = await client.fetch_user(player)
         await user.send("Enter a Prompt:")
-    asyncio.wait(10)
+    await asyncio.sleep(30)
     for player in players:
         user = await client.fetch_user(player)
         messages = [message async for message in user.history(limit=1)]
         latestmessage = messages[0]
-        promptPath = "temp/" + "prompt" + player + ".txt"
+        promptPath = "temp/" + "prompt_" + player + "_0.txt"
         promptFile = open(promptPath, 'w')
         promptFile.write(str(latestmessage.content))
+        promptFile.close()
+    for i in range(len(players)):
+        if i == len(players)-1:
+            user = await client.fetch_user(players[0])
+        else:
+            user = await client.fetch_user(players[i+1])
+        prevPromptPath = "temp/" + "prompt_" + players[i] + "_0.txt"
+        prevPrompt = open(prevPromptPath, 'r')
+        prompt = prevPrompt.read()
+        await user.send("Your prompt is " + prompt)
+
+async def ask_for_prompt(players, round):
+    for player in players:
+        user = await client.fetch_user(player)
+        await user.send("What is this image:")
+    await asyncio.sleep(30)
+    for player in players:
+        user = await client.fetch_user(player)
+        messages = [message async for message in user.history(limit=1)]
+        latestmessage = messages[0]
+        promptPath = "temp/" + "prompt_" + player + "_" + str(round) + ".txt"
+        promptFile = open(promptPath, 'w')
+        promptFile.write(str(latestmessage.content))
+        promptFile.close()
+    for i in range(len(players)):
+        if i == len(players)-1:
+            user = await client.fetch_user(players[0])
+        else:
+            user = await client.fetch_user(players[i+1])
+        prevPromptPath = "temp/" + "prompt_" + players[i] + "_" + str(round) + ".txt"
+        prevPrompt = open(prevPromptPath, 'r')
+        prompt = prevPrompt.read()
+        await user.send("Your prompt is " + prompt)
+
 
 
 
