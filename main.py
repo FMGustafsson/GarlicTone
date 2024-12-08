26eabb31e5e1981868c2240b6a32087680327c9a
FMGustafsson <marcus.gustafsson@hotmail.co.uk>
2024-12-08 11:14:08 +0000

    Add senddm

--- main.py
+++ main.py
@@ -20,8 +20,11 @@ async def on_ready():  #  Called when internal cache is loaded
     channel_id =1315251666349588492
     channel = client.get_channel(channel_id) #  Gets channel from internal cache
     await channel.send("hello world") #  Sends message to channel
+    await send_dm('<@598109867382931476>', "Tom isn't Pog")
 
-
-
+@client.event
+async def send_dm(userID, message):
+    user = await client.fetch_user(userID)
+    await user.send(message)
 
 client.run(TOKEN)
