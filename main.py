2ae54c9e4e00d203c44fa2db1b6fa3f4511f4e05
Origami-Sensei <55271787+Origami-Sensei@users.noreply.github.com>
2024-12-08 10:54:22 +0000

    improved basic bot code

--- main.py
+++ main.py
@@ -6,11 +6,22 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+print(TOKEN)
 
-client = discord.Client()
+client = discord.Client(intents=discord.Intents.default())
+
+#@client.event
+#async def on_ready():
+#    print(f'{client.user} has connected to Discord!')
 
 @client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+async def on_ready():  #  Called when internal cache is loaded
+    #channel = discord.utils.get(client.get_all_channels(), name=name_channel)
+    channel_id =1315251666349588492
+    channel = client.get_channel(channel_id) #  Gets channel from internal cache
+    await channel.send("hello world") #  Sends message to channel
+
+
+
 
 client.run(TOKEN)
